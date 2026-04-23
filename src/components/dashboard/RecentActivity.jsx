22604fb253850f9df6d96d2d37bccf305dd154f8
Maryam Mirzada <marimmirzada@gmail.com>
2026-04-23 18:42:12 +0430

    added the some changes

--- src/components/dashboard/RecentActivity.jsx
+++ src/components/dashboard/RecentActivity.jsx
@@ -1,32 +1,20 @@
-import { Card, CardContent, Typography } from "@mui/material";
-import { useLanguage } from "../../i18n/useLanguage";
+import { Card, CardContent, Typography, Box } from "@mui/material";
+import GoalCard from "../goal/form/GoalCard"
 
-const RecentActivity = ({ activities }) => {
-  const { t } = useLanguage();
-
+const RecentActivity = ({ activities = [] }) => {
   return (
-    <Card
-      sx={{
-        mb: 2,
-        bgcolor: "background.paper",
-        color: "text.primary",
-        backgroundImage: "none",
-        boxShadow: 3,
-      }}
-    >
+    <Card sx={{ mb: 2 }}>
       <CardContent>
-        <Typography variant="h6">
-          {t("recentActivity")}
-        </Typography>
+        <Typography variant="h6">Recent Activity</Typography>
 
         {activities.length === 0 ? (
-          <Typography color="text.secondary">
-            {t("noActivity")}
-          </Typography>
+          <Typography color="text.secondary">No Activity</Typography>
         ) : (
-          activities.map((a, i) => (
-            <Typography key={i}>{a}</Typography>
-          ))
+          <Box className="grid grid-cols-2 gap-4 mt-3">
+            {activities.map((goal, index) => (
+              <GoalCard key={index} goal={goal} />
+            ))}
+          </Box>
         )}
       </CardContent>
     </Card>
